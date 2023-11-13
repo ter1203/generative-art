df2bef78b88976d664956d64f01ff3d77e9dbaf4
ter1203 <terry.hopper1203@gmail.com>
2023-11-13 15:40:38 -0500

    fix: accessibility

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 
-import GenerativeArt from './GenerativeArt';
+import GenerativeArt from "./GenerativeArt";
 
 function App() {
   const [numOfLines, setNumOfLines] = React.useState(5);
@@ -12,15 +12,15 @@ function App() {
       - froot-loops
       - spooky
   */
-  const [colorTheme, setColorTheme] = React.useState('basic');
+  const [colorTheme, setColorTheme] = React.useState("basic");
 
   /*
     Accepted values:
       - circles
       - polygons
   */
-  const [shape, setShape] = React.useState('circles');
-  
+  const [shape, setShape] = React.useState("circles");
+
   return (
     <>
       <GenerativeArt
@@ -28,16 +28,13 @@ function App() {
         colorTheme={colorTheme}
         shape={shape}
       />
-      
+
       <form>
         <fieldset>
           <legend>Controls</legend>
           <div className="row">
             <div className="col">
-              <label
-                htmlFor="num-of-lines"
-                className="control-heading"
-              >
+              <label htmlFor="num-of-lines" className="control-heading">
                 Number of Lines:
               </label>
               <input
@@ -45,48 +42,53 @@ function App() {
                 type="range"
                 min="1"
                 max="15"
+                value={numOfLines}
+                onChange={(e) => setNumOfLines(e.target.value)}
               />
-          	</div>
+            </div>
           </div>
           <div className="row">
             <div className="col">
-              <label
-                className="control-heading"
-                htmlFor="color-theme"
-              >
+              <label className="control-heading" htmlFor="color-theme">
                 Color Theme:
               </label>
-              <select id="color-theme">
-                <option value="basic">
-                  Basic
-                </option>
-                <option value="monochrome">
-                  Monochrome
-                </option>
-                <option value="froot-loops">
-                  Froot Loops
-                </option>
-                <option value="spooky">
-                  Spooky Night
-                </option>
+              <select
+                id="color-theme"
+                value={colorTheme}
+                onChange={(e) => setColorTheme(e.target.value)}
+              >
+                <option value="basic">Basic</option>
+                <option value="monochrome">Monochrome</option>
+                <option value="froot-loops">Froot Loops</option>
+                <option value="spooky">Spooky Night</option>
               </select>
             </div>
             <div className="col">
-              <div className="control-heading">
-                Shape:
-              </div>
+              <div className="control-heading">Shape:</div>
               <div className="radio-wrapper">
                 <div className="radio-option">
-                  <input type="radio" />
-                  <label>
-                    Circles
-                  </label>
+                  <input
+                    type="radio"
+                    checked={shape === "circles"}
+                    onChange={(e) =>
+                      setShape(e.target.checked ? "circles" : "polygons")
+                    }
+                    name="shape"
+                    id="shape-circles"
+                  />
+                  <label htmlFor="shape-circles">Circles</label>
                 </div>
                 <div className="radio-option">
-                  <input type="radio" />
-                  <label>
-                    Polygons
-                  </label>
+                  <input
+                    type="radio"
+                    checked={shape === "polygons"}
+                    onChange={(e) =>
+                      setShape(e.target.checked ? "polygons" : "circles")
+                    }
+                    name="shape"
+                    id="shape-polygons"
+                  />
+                  <label htmlFor="shape-polygons">Polygons</label>
                 </div>
               </div>
             </div>
